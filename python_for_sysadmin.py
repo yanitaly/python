6d8a7ddc2edbca65871572eaa58d4612e3dc4c0c
Xiaoning Yan <xiaoning.yan8@hotmail.com>
2023-03-16 16:55:09 +0100

    dummy change

--- python_for_sysadmin.py
+++ python_for_sysadmin.py
@@ -14,7 +14,7 @@ print(f"Timer stopped at {strftime('%X', starttime)}")
 difference = mktime(stoptime) - mktime(starttime)
 print(f"Total time: {difference} seconds")
 
-## Others changes
+## Others changes TBD
 # loop through yaml nested data
 # disks = ['disk%d' % i for i in range(yaml_file['parent_key']['a_key'])]
 # for t_disk in disks 
