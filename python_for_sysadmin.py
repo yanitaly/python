c873c70b2db9bbaedd0c37c6b981aaf9b6a326ec
Xiaoning Yan <xiaoning.yan8@hotmail.com>
2023-03-16 16:48:32 +0100

    minor fix

--- python_for_sysadmin.py
+++ python_for_sysadmin.py
@@ -1,9 +1,9 @@
-!#/usr/bin/env python3.6 
+#!/usr/bin/env python3.6 
 ### ACG online training: Python 3 Scripting for System Administrators
 
 ## chapter7: using std lib 
 
-!#/usr/bin/env python3.6 
+#!/usr/bin/env python3.6 
 from time import localtime, strftime, mktime
 
 starttime = localtime()
@@ -12,9 +12,10 @@ input("Press 'Enter' to stop timer ")
 stoptime = localtime()
 print(f"Timer stopped at {strftime('%X', starttime)}")
 difference = mktime(stoptime) - mktime(starttime)
-print(f"Total time: {difference}" seconds)
+print(f"Total time: {difference} seconds")
 
 ## Others 
 # loop through yaml nested data
-disks = ['disk%d' % i for i in range(yaml_file['parent_key']['a_key'])]
-for t_disk in disks:
+# disks = ['disk%d' % i for i in range(yaml_file['parent_key']['a_key'])]
+# for t_disk in disks:
+  
